9e889b608bd22d7e8195aa5f41f766b74d56b301
Anurag M K <109131413+Anurag-M-K@users.noreply.github.com>
2024-08-25 15:44:07 +0530

    database connection updaetd

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,12 +2,15 @@ import mongoose from "mongoose";
 
 const connectDb = async () => {
   const uri: any = process.env.DATABASE_URL;
-
+  const connectionParams:any = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
   try {
-    const connection = await mongoose.connect(uri);
+    const connection = await mongoose.connect(uri, connectionParams);
     console.log(`🟢 Mongo db connected:`, connection.connection.host);
   } catch (error) {
-    console.log("mongodb connection error",error);
+    console.log("mongodb connection error", error);
     process.exit(1);
   }
 };
